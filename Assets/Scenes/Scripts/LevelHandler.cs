fb0c78617146a3fa0de2cf7f5570ab7e1b41e4d2
xXCringeUsernameXx <karsonyurchak@gmail.com>
2025-07-08 14:34:08 -0700

    Add files via upload

---
+++ Assets/Scenes/Scripts/LevelHandler.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+
+public class LevelHandler : MonoBehaviour
+{
+    public GameObject[] levels;
+    public PlayerControl playerControl;
+    public TextMeshProUGUI scoreText;
+    public Button restartText;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        playerControl = GameObject.Find("PlayerPlaceholder").GetComponent<PlayerControl>();
+        Debug.Log(playerControl.crntLvl);
+    }
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        levels[playerControl.crntLvl].SetActive(true);
+        if (playerControl.isGameOver == true)
+        {
+            restartText.gameObject.SetActive(true);
+        }
+    }
+}
